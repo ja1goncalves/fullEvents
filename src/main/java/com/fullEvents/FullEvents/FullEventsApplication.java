99213944a92caf4e5eb91a19cf2eb0ec0f475686
João Paulo F. Gonçalves da Silva <joaosilva@mangue3.com>
2019-11-18 17:07:25 -0300

    first commit

---
+++ src/main/java/com/fullEvents/FullEvents/FullEventsApplication.java
@@ -0,0 +1,13 @@
+package com.fullEvents.FullEvents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullEventsApplication.class, args);
+	}
+
+}
